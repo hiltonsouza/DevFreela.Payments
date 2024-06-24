65d78b0088506f0a4b1019e34ef7ed257c040a5f
Hilton Paiva de Sousa <h_iltonsouza@outlook.com>
2024-06-24 17:54:48 -0300

    Add project files.

---
+++ DevFreela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,17 @@
+﻿using DevFreela.Payments.API.Models;
+
+namespace DevFreela.Payments.API.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public PaymentService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public Task<bool> Process(PaymentInfoInputModel paymentInfoInputModel)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
